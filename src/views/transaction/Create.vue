4672a9f0e8962664cf03d6512fb053461769eb6a
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2021-11-17 09:35:43 +0700

    edit

--- src/views/transaction/Create.vue
+++ src/views/transaction/Create.vue
@@ -2,40 +2,38 @@
   <div class="container my-5">
     <div class="row justify-content-center">
       <div class="col-8">
-        <router-link to="/" class="btn btn-primary btn-sm rounded shadow mb-3"
-          >Back</router-link
-        >
+        <router-link to="/" class="btn btn-primary btn-sm rounded shadow mb-3">Back</router-link>
         <div class="card rounded shadow">
           <div class="card-header">Create Transaction</div>
           <div class="card-body">
             <form @submit.prevent="store()">
               <div class="mb-3">
-                <label for="" class="form-label">Title</label>
+                <label for class="form-label">Title</label>
                 <input type="text" class="form-control" v-model="transaction.title" />
-                <div class="text-danger">Validation Message</div>
+                <div v-if="validation.title" class="text-danger">{{ validation.title[0] }}</div>
               </div>
               <div class="mb-3">
-                <label for="" class="form-label">Amount</label>
+                <label for class="form-label">Amount</label>
                 <input type="number" class="form-control" v-model="transaction.amount" />
-                <div class="text-danger">Validation Message</div>
+                <div v-if="validation.amount" class="text-danger">{{ validation.amount[0] }}</div>
               </div>
               <div class="mb-3">
-                <label for="" class="form-label">Time</label>
+                <label for class="form-label">Time</label>
                 <input
                   type="text"
                   class="form-control"
                   placeholder="yyyy-mm-dd hh:mm:ss"
                   v-model="transaction.time"
                 />
-                <div class="text-danger">Validation Message</div>
+                <div v-if="validation.time" class="text-danger">{{ validation.time[0] }}</div>
               </div>
               <div class="mb-3">
-                <label for="" class="form-label">type</label>
-                <select id="" class="form-select" v-model="transaction.type">
+                <label for class="form-label">type</label>
+                <select id class="form-select" v-model="transaction.type">
                   <option value="expense">Expense</option>
                   <option value="revenue">Revenue</option>
                 </select>
-                <div class="text-danger">Validation Message</div>
+                <div v-if="validation.type" class="text-danger">{{ validation.type[0] }}</div>
               </div>
               <button class="btn btn-outline-primary">Submit</button>
             </form>
